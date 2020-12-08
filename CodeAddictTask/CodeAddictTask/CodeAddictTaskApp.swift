de870285f93939eff0d3c3201fa4a043684b3ad7
kubolko <70199321+kubolko@users.noreply.github.com>
2020-12-08 21:42:18 +0100

    Add files via upload
    Inital Commit

---
+++ CodeAddictTask/CodeAddictTask/CodeAddictTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeAddictTaskApp.swift
+//  CodeAddictTask
+//
+//  Created by Jakub Sumionka on 08/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct CodeAddictTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
